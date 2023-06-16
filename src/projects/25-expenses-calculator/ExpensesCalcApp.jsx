5220a9c2f690972e635b536c4bf27cbc95f6ee00
Joshua Hill <jhill42015@gmail.com>
2023-06-15 19:22:21 -0500

    Expense calc app added

--- src/projects/25-expenses-calculator/ExpensesCalcApp.jsx
+++ src/projects/25-expenses-calculator/ExpensesCalcApp.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Title from '../components/Title';
 import ExpensesForm from './components/ExpensesForm';
 import ExpensesList from './components/ExpensesList';
+import { BudgetStyle } from './components/styles/BudgetStyle';
+import { v4 as uuidV4 } from 'uuid';
+
+const initialExpense = localStorage.getItem('expenses')
+  ? JSON.parse(localStorage.getItem('expenses'))
+  : [];
 
 export default function ExpensesCalcApp() {
+  const [expenses, setExpenses] = useState(initialExpense);
+  const [date, setDate] = useState('');
+  const [amount, setAmount] = useState('');
+  const [charge, setCharge] = useState('');
+  const [budget, setBudget] = useState('');
+  const [id, setId] = useState(0);
+
+  let inputBudget = useRef(null);
+
+  useEffect(() => {
+    inputBudget.current.value === '' && inputBudget.current.focus();
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+  }, [expenses]);
+
+  const changeBudget = (e) => {
+    setBudget(inputBudget.current.value);
+  };
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+  const handleDate = (e) => {
+    setDate(e.target.value);
+  };
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+  let edit;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (date !== '' && charge !== '' && amount > 0) {
+      if (edit) {
+        let tempExpense = expenses.map((item) => {
+          return item.id === id ? { ...item, date, charge, amount } : item;
+        });
+        setExpenses(tempExpense);
+      } else {
+        const singleExpense = { id: uuidV4(), date, charge, amount };
+        setExpenses([...expenses, singleExpense]);
+      }
+    }
+  };
   return (
     <main className="container">
       <Title text={'Expenses Calculator'} />
@@ -17,21 +64,41 @@ export default function ExpensesCalcApp() {
         }}
       >
         <aside>
-          <ExpensesForm />
-          <section className='card mt-2 bt-primary text-light'>
+          <ExpensesForm
+            date={date}
+            charge={charge}
+            amount={amount}
+            handleDate={handleDate}
+            handleCharge={handleCharge}
+            handleSubmit={handleSubmit}
+            handleAmount={handleAmount}
+          />
+
+          <section className="card mt-2 bg-primary text-light text-right">
             <div className="card-body">
-                {/* <BudgetStyle> */}
+              <BudgetStyle>
                 <h3>Budget : $</h3>
-                <input type="number" value="" />
-                {/* </BudgetStyle> */}
-                <h3 className="mb-1">Total expenses: $ </h3>
-                 {/* Calc economies */}
-                 <h3>Economies: $</h3>
+                <input
+                  type="number"
+                  value={budget}
+                  onChange={changeBudget}
+                  ref={inputBudget}
+                />
+              </BudgetStyle>
+              <h3 className="mb-1">Total expenses: $ </h3>
+              {/* Calc economies */}
+              <h3>Economies: $</h3>
             </div>
           </section>
         </aside>
+        <section>
+          <ExpensesList
+            expenses={expenses}
+            // handleDelete={handleDelete}
+            // handleEdit={handleEdit}
+          />
+        </section>
       </section>
-      <section><ExpensesList/></section>
     </main>
   );
 }
--- src/projects/25-expenses-calculator/components/ExpensesList.jsx
+++ src/projects/25-expenses-calculator/components/ExpensesList.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { MdDelete } from 'react-icons/md';
+import ExpenseItem from './ExpenseItem';
 
-export default function ExpensesList({ clearItems, expense= [] }) {
+export default function ExpensesList({ expenses, clearItems, handleDelete, handleEdit }) {
   return (
     <>
-      <ul className="list">{/* <ExpenseItem/> */}</ul>
-      {expense.length > 0 && (
+      <ul className="list">
+      {expenses.map((expense)=> {
+        return <ExpenseItem key={expense.id} expense={expense} handleDelete={handleDelete} handleEdit={handleEdit}/>
+      })}
+      </ul>
+      {expenses.length > 0 && (
         <button className="btn btn-danger" onClick={clearItems}>
           <MdDelete /> Clear all expenses
         </button>
